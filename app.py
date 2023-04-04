2002e2462d0b992a57feeb19c25db1098cbcb547
mcastre1 <miguelcastrejon@mail.weber.edu>
2023-04-03 20:42:23 -0600

    Testing out database access through heroku.

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
+from models import Inventory, Job, Sink
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    
+    sink = Sink("Stainless Steel Single Bowl Kitchen Sink")
+    sink.insert()
+
+    sink = Sink("Rect White Porcelain Bathroom Sink")
+    sink.insert()
+
+    sink = Sink("Oval White Porcelain Bathroom Sink")
+    sink.insert()
+
+
     return render_template('pages/home.html')
