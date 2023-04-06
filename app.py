da964be770e55411da1525f70b8b608eca3e1a2d
mcastre1 <miguelcastrejon@mail.weber.edu>
2023-04-05 21:25:22 -0600

    Added basic form for job creation.

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
+# Returns a list of all jobs in database.
 @app.route('/')
 def home():
     return render_template('pages/home.html', sinks=(sink.format() for sink in Sink.query.all()))
+
+@app.route('/job', methods=["POST"])
+def create_job():
+    return render_template('pages/home.html')
+
+@app.route('/job', methods=["GET"])
+def create_job_form():
+    return render_template('pages/create_job.html')
