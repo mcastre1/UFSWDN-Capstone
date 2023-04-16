2b79fd061545144c5c00acee460dee66047c2858
mcastre1 <miguelcastrejon@mail.weber.edu>
2023-04-16 05:37:58 -0600

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+
+export DATABASE_URL="postgres://kubnlmsvwcgglh:4f8d755351f8a8c68d7c7303607081031b657529f69c5142063ee8108b87c724@ec2-34-236-103-63.compute-1.amazonaws.com:5432/d9dsh44ov6ktm5"
+export APP_SECRET_KEY="something unique and secret key"
+export AUTH0_CLIENT_ID="PYEPrbcSnWPlTrIAjvTjp2caniJnSotT"
+export AUTH0_CLIENT_SECRET="yuMq7yBG_y2ht6LXkY_GBlG-mqvP5fPCDfaW5k2Q-2qPCbop1zGL08ad6pmPqWIF"
+export AUTH0_DOMAIN="dev-nmyxk7hftomeflrd.us.auth0.com"
+export AUTH0_AUDIENCE="capstoneAPI"
+export AUTH0_CALLBACK="http://127.0.0.1:5000/"
